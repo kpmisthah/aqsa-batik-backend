e23502655d8d9c428577a569a82fbd300cce76bc
kpmisthah <kpmisthah6@gmail.com>
2026-08-05 17:11:15 +0530

    feat(product): ensure up-to-date route definitions for products

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getProducts, getProductById, createProduct, updateProduct, toggleBlockProduct, deleteProduct, createBulkProducts } from '../controllers/ProductController.js';
+import { getProducts, getProductById, createProduct, updateProduct, toggleBlockProduct, deleteProduct, createBulkProducts, bulkUpdateInventory } from '../controllers/ProductController.js';
 
 const router = express.Router();
 
 router.route('/').get(getProducts).post(createProduct);
 router.route('/bulk').post(createBulkProducts);
+router.route('/inventory').put(bulkUpdateInventory);
 router.route('/:id').get(getProductById).put(updateProduct).delete(deleteProduct);
 router.route('/:id/toggle-block').patch(toggleBlockProduct);
 
